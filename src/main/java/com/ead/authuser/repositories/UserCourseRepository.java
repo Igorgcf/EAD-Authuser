ed397aa575b672aac9d975659c3d6c8e30bef377
igorgcf <igorTecnologia@outlook.com>
2025-02-27 19:21:41 -0300

    User Course Repository

--- src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
+++ src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UserCourseRepository extends JpaRepository<UserCourse, UUID> {
+
+    boolean existsByUserIdAndCourseId(UUID userId, UUID id);
 }
