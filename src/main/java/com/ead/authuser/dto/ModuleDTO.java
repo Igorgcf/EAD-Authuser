15ef074da61ca1287d1f7f7224acebded5ec7d23
igorgcf <igorTecnologia@outlook.com>
2025-02-27 19:20:37 -0300

    Module DTO

--- src/main/java/com/ead/authuser/dto/ModuleDTO.java
+++ src/main/java/com/ead/authuser/dto/ModuleDTO.java
@@ -1,11 +1,13 @@
 package com.ead.authuser.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class ModuleDTO {
 
