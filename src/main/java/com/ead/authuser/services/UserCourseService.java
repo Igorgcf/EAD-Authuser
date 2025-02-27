17c7ad98ed8f79a6619334b81175241ec6d3372e
igorgcf <igorTecnologia@outlook.com>
2025-02-27 19:21:53 -0300

    User Course Service

--- src/main/java/com/ead/authuser/services/UserCourseService.java
+++ src/main/java/com/ead/authuser/services/UserCourseService.java
@@ -1,4 +1,11 @@
 package com.ead.authuser.services;
 
+import com.ead.authuser.dto.CourseDTO;
+import com.ead.authuser.dto.UserCourseDTO;
+
+import java.util.UUID;
+
 public interface UserCourseService{
+
+    UserCourseDTO saveSubscriptionUserInCourse(UUID userId, CourseDTO dto);
 }
