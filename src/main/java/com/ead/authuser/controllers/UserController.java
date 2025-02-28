aa3dd6ec195c8fc6c5185d82b3535fc85bbd99ad
igorgcf <igorTecnologia@outlook.com>
2025-02-28 01:44:25 -0300

    User Controller

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.ead.authuser.dto.UserDTO;
 import com.ead.authuser.services.impl.UserServiceImpl;
 import com.ead.authuser.specification.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -52,40 +53,50 @@ public class UserController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<UserDTO> findById(@PathVariable UUID id){
+    public ResponseEntity<UserDTO> findById(@PathVariable (value = "id") UUID id ){
 
         UserDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<UserDTO> update(@PathVariable UUID id,
-                                          @RequestBody @Validated(UserDTO.UserView.UserPut.class)
-                                          @JsonView(UserDTO.UserView.UserPut.class) UserDTO dto){
+    public ResponseEntity<UserDTO> update(@PathVariable (value = "id") UUID id,
+                                          @Validated(UserDTO.UserView.UserPut.class)
+                                          @JsonView(UserDTO.UserView.UserPut.class)
+                                          @RequestBody @Valid UserDTO dto){
 
         dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
 
+    @PutMapping(value = "/{id}/cpf")
+    public ResponseEntity<UserDTO> updateCpf(@PathVariable (value = "id") UUID id,
+                                             @RequestBody @Validated(UserDTO.UserView.CpfPut.class)
+                                             @JsonView(UserDTO.UserView.CpfPut.class) @Valid UserDTO dto){
+
+        dto = service.updateCpf(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
     @PutMapping(value = "/{id}/password")
-    public ResponseEntity<Object> updatePassword(@PathVariable UUID id,
+    public ResponseEntity<Object> updatePassword(@PathVariable (value = "id") UUID id,
                                                  @RequestBody @Validated(UserDTO.UserView.PasswordPut.class)
-                                                 @JsonView(UserDTO.UserView.PasswordPut.class) UserDTO dto){
+                                                 @JsonView(UserDTO.UserView.PasswordPut.class) @Valid UserDTO dto){
         service.updatePassword(id, dto);
         return ResponseEntity.ok().body("Password updated successfully!");
     }
 
     @PutMapping(value = "/{id}/image")
-    public ResponseEntity<Object> updateImage(@PathVariable UUID id,
+    public ResponseEntity<Object> updateImage(@PathVariable (value = "id") UUID id,
                                               @RequestBody @Validated(UserDTO.UserView.ImagePut.class)
-                                              @JsonView(UserDTO.UserView.ImagePut.class) UserDTO dto){
+                                              @JsonView(UserDTO.UserView.ImagePut.class) @Valid UserDTO dto){
 
         dto = service.updateImage(id, dto);
         return ResponseEntity.ok().body("Image updated successfully!");
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Object> deleteById(@PathVariable UUID id){
+    public ResponseEntity<Object> deleteById(@PathVariable (value = "id") UUID id){
 
         service.deleteById(id);
         return ResponseEntity.ok().body("User deleted successfully!");
