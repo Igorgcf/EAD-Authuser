92c2fe994fddbdb2b0b9dbfa7a53321fbeca0021
igorgcf <igorTecnologia@outlook.com>
2025-03-02 11:51:54 -0300

    User Course DTO

--- src/main/java/com/ead/authuser/dto/UserCourseDTO.java
+++ src/main/java/com/ead/authuser/dto/UserCourseDTO.java
@@ -1,6 +1,7 @@
 package com.ead.authuser.dto;
 
 import com.ead.authuser.models.User;
+import com.ead.authuser.models.UserCourse;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
@@ -21,5 +22,12 @@ public class UserCourseDTO {
         this.id = id;
         this.courseId = courseId;
         this.userDTO = new UserDTO(user);
+    }
+
+    public UserCourseDTO(UserCourse entity){
+
+        id = entity.getId();
+        courseId = entity.getCourseId();
+        userDTO = new UserDTO(entity.getUser());
     }
 }
