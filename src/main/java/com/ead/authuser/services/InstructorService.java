56d54f2714b769f564194befbf7ee6a448884825
Igorgcf <igorTecnologia@outlook.com>
2025-11-07 16:32:23 -0300

    Instructor Service

--- src/main/java/com/ead/authuser/services/InstructorService.java
+++ src/main/java/com/ead/authuser/services/InstructorService.java
@@ -1,6 +1,7 @@
 package com.ead.authuser.services;
 
 import com.ead.authuser.dto.UserDTO;
+import com.ead.authuser.models.User;
 
 import java.util.UUID;
 
@@ -8,17 +9,29 @@ public interface InstructorService {
 
     UserDTO insertInstructor(UserDTO dto);
 
-    void updateUserToInstructor(UUID id);
+    UserDTO save (UserDTO dto);
+
+    UserDTO updateUserToInstructor(UUID id);
+
+    UserDTO updaterUserToInstructor(UUID id);
 
     UserDTO findById(UUID id);
 
     UserDTO updateInstructor(UUID id, UserDTO dto);
 
+    UserDTO updaterInstructor(UUID id, UserDTO dto);
+
     UserDTO updateCpf(UUID id, UserDTO dto);
+
+    UserDTO updaterCpf(UUID id, UserDTO dto);
 
     void updatePassword(UUID id, UserDTO dto);
 
-    void updateImage(UUID id, UserDTO dto);
+    UserDTO updateImage(UUID id, UserDTO dto);
+
+    void updaterImage(UUID id, UserDTO dto);
 
     void deleteById(UUID id);
+
+    void deleterById(UUID id);
 }
