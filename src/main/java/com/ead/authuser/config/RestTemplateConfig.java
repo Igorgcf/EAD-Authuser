a8e22b68394d2cb699b412d821627d32e82a81be
Igorgcf <igorTecnologia@outlook.com>
2025-11-14 14:50:20 -0300

    Rest Template Config

--- src/main/java/com/ead/authuser/config/RestTemplateConfig.java
+++ src/main/java/com/ead/authuser/config/RestTemplateConfig.java
@@ -4,15 +4,25 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
 
 @Configuration
 public class RestTemplateConfig {
+
+    static final int TIMEOUT = 5000;
 
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder){
 
-        return builder.build();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(TIMEOUT);
+        factory.setReadTimeout(TIMEOUT);
+
+        return builder
+            .requestFactory(() -> factory)
+            .build();
     }
 }
