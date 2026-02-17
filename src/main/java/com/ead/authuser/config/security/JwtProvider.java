4c6368e7ead528c709386f66ba5a12ff4f02d605
Igorgcf <igorTecnologia@outlook.com>
2026-02-17 18:49:01 -0300

    JWT provider

---
+++ src/main/java/com/ead/authuser/config/security/JwtProvider.java
@@ -0,0 +1,79 @@
+package com.ead.authuser.config.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Log4j2
+@Component
+public class JwtProvider {
+
+    private Key key;
+
+    @Value("${ead.auth.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${ead.auth.jwtExpirationMs}")
+    private int jwtExpirationsMs;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateJwt(Authentication authentication) {
+
+        UserDetailsImpl userPrincipal =
+                (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(
+                        System.currentTimeMillis() + jwtExpirationsMs
+                ))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public boolean validateJwt(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+        return true;
+        }catch(SignatureException e){
+            log.error("Invalid JWT signature: {} ", e.getMessage());
+        }catch(MalformedJwtException e){
+            log.error("Invalid JWT token: {} ", e.getMessage());
+        }catch(ExpiredJwtException e){
+            log.error("JWT token is expired: {} ", e.getMessage());
+        }catch(UnsupportedJwtException e){
+            log.error("JWT token is unsupported: {} ", e.getMessage());
+        }catch(IllegalArgumentException e){
+            log.error("JWT claims string is empty: {} ", e.getMessage());
+        }
+        return false;
+    }
+
+    public String getUsernameFromJwt(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
+
