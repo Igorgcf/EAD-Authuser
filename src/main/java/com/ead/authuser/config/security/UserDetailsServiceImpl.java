c63c7bcda913986fd7fe5403bc456153e5fe3cff
Igorgcf <igorTecnologia@outlook.com>
2026-05-09 18:47:49 -0300

    User Details Service Impl

--- src/main/java/com/ead/authuser/config/security/UserDetailsServiceImpl.java
+++ src/main/java/com/ead/authuser/config/security/UserDetailsServiceImpl.java
@@ -5,12 +5,15 @@ import com.ead.authuser.models.User;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -18,11 +21,20 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository repository;
 
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         Optional<User> obj = repository.findByUsername(username);
         User entity = obj.orElseThrow(() -> new ResourceNotFoundException("User not found with username: " + username));
-        return UserDetailsImpl.build(new UserDTO(entity));
+        return UserDetailsImpl.buildFromEntity(entity);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(UUID userId) throws AuthenticationCredentialsNotFoundException {
+
+        Optional<User> obj = repository.findById(userId);
+        User entity = obj.orElseThrow(() -> new ResourceNotFoundException("User not found with userId: " + userId));
+        return UserDetailsImpl.buildFromEntity(entity);
     }
 }
