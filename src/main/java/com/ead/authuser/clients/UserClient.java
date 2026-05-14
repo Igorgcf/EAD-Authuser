1cc979b6b758b56fa7d1ea4ecce2095eb31b15ab
Igorgcf <igorTecnologia@outlook.com>
2026-05-14 14:30:58 -0300

    User Client

--- src/main/java/com/ead/authuser/clients/UserClient.java
+++ src/main/java/com/ead/authuser/clients/UserClient.java
@@ -2,18 +2,13 @@ package com.ead.authuser.clients;
 
 import com.ead.authuser.dto.CourseDTO;
 import com.ead.authuser.dto.ResponsePageDTO;
-import com.ead.authuser.dto.UserDTO;
-import com.ead.authuser.models.User;
 import com.ead.authuser.services.UtilsService;
-import com.ead.authuser.services.exceptions.BadRequestException;
 import com.ead.authuser.services.exceptions.ResourceNotFoundException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -24,9 +19,6 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Log4j2
