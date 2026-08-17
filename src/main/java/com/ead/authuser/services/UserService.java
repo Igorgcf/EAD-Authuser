cc62716701219fc39ccd0628eb756d27014b8fea
Igorgcf <igorTecnologia@outlook.com>
2026-08-16 23:38:47 -0300

    User Service

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -2,6 +2,7 @@ package com.ead.authuser.services;
 
 import com.ead.authuser.dto.JwtDTO;
 import com.ead.authuser.dto.UserDTO;
+import com.ead.authuser.enums.PaymentControl;
 import com.ead.authuser.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,15 +16,21 @@ public interface UserService {
 
     UserDTO findById(UUID id);
 
+    UserDTO findByIdInternal(UUID id);
+
     UserDTO insert(UserDTO dto);
 
     UserDTO save(UserDTO dto);
 
     JwtDTO authentication (UserDTO dto);
 
+    UserDTO insertAdmin(UserDTO dto);
+
     UserDTO update(UUID id, UserDTO dto);
 
     UserDTO updater(UUID id, UserDTO dto);
+
+    UserDTO updateAfterPayment(UUID id, PaymentControl paymentControl);
 
     UserDTO updateCpf (UUID id, UserDTO dto);
 
